6955ead134d8dfe962034e046c6623bcfca80737
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-03-09 08:16:24 +0000

    [update] follow/follower

--- Bookers2-remake/app/models/user.rb
+++ Bookers2-remake/app/models/user.rb
@@ -8,7 +8,25 @@ class User < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :followings, through: :relationships, source: :followed
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+  
 end
