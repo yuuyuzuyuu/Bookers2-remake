92e0fa0f45822f7e16f880fb989bdeb4ede41a6d
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-03-09 01:04:04 +0000

    Start

---
+++ Bookers2-remake/app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite.destroy
+    redirect_to request.referer
+  end
+end
